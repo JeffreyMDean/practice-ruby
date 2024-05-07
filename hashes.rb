2112514dbc356d7a99e02a94cf8477f126a6ee20
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-05-07 15:36:20 -0400

    put attr_reader inside the class hashes.rb

--- hashes.rb
+++ hashes.rb
@@ -15,27 +15,22 @@ book = {:color => "green", :price => 7}
 
 
 class Item
+  attr_reader :color, :price
+  
   def initialize(input_color, input_price)
     @color = input_color
     @price = input_price
   end
 
-  def color
-    @color
-  end
-
-  def price
-    @price
-  end
-
   def print_info
     puts "#{color} #{price}"
   end
-
-  item1 = Item.new("red", 28)
-  item2 = Item.new("purple", 50)
-
-  p item1, item2
-
 end
+
+item1 = Item.new("red", 28)
+item2 = Item.new("purple", 50)
+
+p item1, item2
+
+
 
